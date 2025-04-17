0399bc6ad998c19cd5385da47a85a04ef9925afc
Ilya Yudin <yudin2402@gmail.com>
2025-04-17 17:39:48 +0200

    added mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const selectors = {
+    NAV: '.js-nav',
+    BURGER: '.js-burger',
+  };
+  const classes = {
+    ACTIVE: 'active',
+  }
+
+  const nav = document.querySelector(selectors.NAV);
+  const burger = document.querySelector(selectors.BURGER);
+
+  if(!nav || !burger) {
+    return;
+  }
+
+  burger.addEventListener('click', toggleNav);
+
+  function toggleNav() {
+    if(nav.classList.contains(classes.ACTIVE)) {
+      nav.classList.remove(classes.ACTIVE);
+      burger.classList.remove(classes.ACTIVE);
+    } else {
+      nav.classList.add(classes.ACTIVE);
+      burger.classList.add(classes.ACTIVE);
+    }
+  }
+
+});
