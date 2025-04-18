eca1b31d80fe52a577b3dd06208db3c6b19e0a41
Ilya Yudin <yudin2402@gmail.com>
2025-04-18 11:26:07 +0200

    added footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectors = {
     NAV: '.js-nav',
     BURGER: '.js-burger',
+    BODY: 'body',
   };
   const classes = {
     ACTIVE: 'active',
+    NO_SCROLL: 'no-scroll'
   }
 
   const nav = document.querySelector(selectors.NAV);
   const burger = document.querySelector(selectors.BURGER);
+  const body = document.querySelector(selectors.BODY);
 
   if(!nav || !burger) {
     return;
@@ -20,9 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if(nav.classList.contains(classes.ACTIVE)) {
       nav.classList.remove(classes.ACTIVE);
       burger.classList.remove(classes.ACTIVE);
+      body.classList.remove(classes.NO_SCROLL);
     } else {
       nav.classList.add(classes.ACTIVE);
       burger.classList.add(classes.ACTIVE);
+      body.classList.add(classes.NO_SCROLL);
     }
   }
 });
